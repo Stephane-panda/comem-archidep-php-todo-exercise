6e853d1e94295c9928e759139f17823410d2568d
stephane bondabelo <stephane.bondabelo@heig-vd.ch>
2019-10-09 19:37:59 +0200

    add feature toogle ans synchronize

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $updateQuery = ''; // IMPLEMENT ME
+        $updateQuery = 'INSERT INTO todo VALUES(NULL, \''.$title.'\''; // IMPLEMENT ME
         if(!$db->query($updateQuery)) {
           die(print_r($db->errorInfo(), true));
         }
